a1b8a49de2dbd25605bba2a8d32e8b836f61e4fd
SwathiRamachandran31 <swathiramachandran31@gmail.com>
2025-05-30 12:31:08 +0100

    Exercise 3

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 let clickAction = () =>{
-    let content=document.getElementById('textbox').value;
-    alert(content);
+    let a=parseInt(document.getElementById('a').value);
+    let b=parseInt(document.getElementById('b').value)
+    let n=parseInt(document.getElementById('n').value)
+    let sum=findsum(a,b,n);
+    alert(sum);
+}
+
+let findsum = (num1,num2,range) => {
+    let sum=0;
+    for(let i=1;i<=range;i++){
+        if(i%num1===0|| i%num2===0){
+            //sum=sum+i
+            sum+=i;
+        }
+
+    }
+    return sum;
 }
